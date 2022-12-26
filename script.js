aa7f93da6e5ad5d97e9beb6f29ac070086773789
Ethan Zhang <yixinzhang92@gmail.com>
2022-12-26 13:52:56 +1100

    Update home page structure

--- script.js
+++ script.js
@@ -4,21 +4,19 @@ const homeCarouselTitle = document.getElementsByClassName("carouselTitle")[0];
 
 const homeCarouselText = document.getElementsByClassName("carouselText")[0];
 
-const title0 = "Tax Consulting Services";
-const title1 = "Self-Managed Superannuation Fund";
-const title2 = "Bookkeeping";
-const title3 = "Tax Return Services";
+const title0 = "TAX CONSULTING";
+const title1 = "SELF-MANAGED SUPERFUND";
+const title2 = "BOOKKEEPING";
+const title3 = "TAX RETURNS";
 
-const text0PartA =
-  "Over time your business and personal circumstances will develop and evolving tax legislation will bring new obligations. Your taxation requirements will also change. The right advice at the right time can make all the difference.\r\n";
-const text0PartB =
-  "Our taxation team is skilled in working across a range of industries and businesses including public and private companies, foreign owned entities and family owned businesses, trusts and partnerships.";
+const text0 =
+  "Over time your business and personal circumstances will develop and evolving tax legislation will bring new obligations. Your taxation requirements will also change. The right advice at the right time can make all the difference. Over time your business and personal circumstances will develop and evolving tax legislation will bring new obligations. Your taxation requirements will also change. The right advice at the right time can make all the difference."
 const text1 =
-  "Our team of accredited SMSF Accountants specialise in automation systems, having built our own toolkit on top of the Class Super platform. This allows us to offer a service founded on unparalleled technical excellence while ensuring value in everything we do!";
+  "Our team of accredited SMSF Accountants specialise in automation systems, having built our own toolkit on top of the Class Super platform. This allows us to offer a service founded on unparalleled technical excellence while ensuring value in everything we do! Our team of accredited SMSF Accountants specialise in automation systems, having built our own toolkit on top of the Class Super platform. This allows us to offer a service founded on unparalleled technical excellence!";
 const text2 =
-  "Our Bookkeeping solutions are designed for small to medium sized businesses, from a cross-section of industries. We know that with the right Bookkeeping systems and support, we can get you back to focusing on what matters most to you - building your business by providing solutions to save you time and money, while giving you peace of mind.";
+  "Our Bookkeeping solutions are designed for small to medium sized businesses, from a cross-section of industries. We know that with the right Bookkeeping systems and support, we can get you back to focusing on what matters most to you - building your business by providing solutions to save you time and money, while giving you peace of mind. We know that with the right Bookkeeping systems and support, we can get you back to focusing on what matters most to you.";
 const text3 =
-  "EazyChamp works hard to help everyone with their individual tax returns so that you get the best income tax return you deserve.";
+  "EazyChamp works hard to help everyone with their individual tax returns so that you get the best income tax return you deserve. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio accusamus assumenda aliquid velit doloremque. Obcaecati aspernatur recusandae id provident ullam nesciunt illum? Et, inventore tempore voluptates exercitationem magnam nam sit! Et, inventore tempore voluptates exercitationem magnam nam sit!";
 
 homeCarousel.addEventListener("slide.bs.carousel", (event) => {
   const currentSlide = document
@@ -28,8 +26,7 @@ homeCarousel.addEventListener("slide.bs.carousel", (event) => {
   switch (currentSlide) {
     case "0":
       homeCarouselTitle.textContent = title0;
-      homeCarouselText.innerText = text0PartA;
-      homeCarouselText.innerText += text0PartB;
+      homeCarouselText.innerText = text0;
       break;
     case "1":
       homeCarouselTitle.textContent = title1;
